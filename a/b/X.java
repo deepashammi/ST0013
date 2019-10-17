efe18ef39c5cc212856ac381908c2ec5ec7adf2a
DeepaShammi <deepa.shammi@aurea.com>
2019-10-18 04:57:16 +0530

    Initial commit

---
+++ a/b/X.java
@@ -0,0 +1,16 @@
+package a.b;
+
+public class X {
+    public void y() {
+        int abc = 10;
+        int cde = abc * 5;
+        abc = cde + 7;
+    }
+
+    public void z (){
+        int ab = 10;
+        int cdp = ab * 5;
+        ab = cdp + 7;
+
+    }
+}
